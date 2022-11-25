cb9dd757dc5deaf2171cfc954cd878a548363063
LeozorKrasota <118885914+LeozorKrasota@users.noreply.github.com>
2022-11-25 12:42:00 +0200

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -13,3 +13,4 @@ cd /usr/src && git clone https://github.com/fusionpbx/fusionpbx-install.sh.git
 cd /usr/src/fusionpbx-install.sh/debian
 rm /usr/src/fusionpbx-install.sh/debian/install.sh
 wget https://raw.githubusercontent.com/LeozorKrasota/FusionPBXScripts/main/install.sh
+chmod +x /usr/src/fusionpbx-install.sh/debian/install.sh
