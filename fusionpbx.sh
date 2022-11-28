69224e267d210abaa5039fbf40b654c75ff7b6d4
LeozorKrasota <118885914+LeozorKrasota@users.noreply.github.com>
2022-11-28 10:56:11 +0200

    Update fusionpbx.sh

--- fusionpbx.sh
+++ fusionpbx.sh
@@ -15,22 +15,22 @@ apt-get install -y vim git dbus haveged ssl-cert qrencode
 apt-get install -y ghostscript libtiff5-dev libtiff-tools at
 
 #get the branch
-if [ .$system_branch = .'master' ]; then
-	verbose "Using master"
-	branch=""
-else
-	system_major=$(git ls-remote --heads https://github.com/fusionpbx/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
-	system_minor=$(git ls-remote --tags https://github.com/fusionpbx/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
-	system_version=$system_major.$system_minor
-	verbose "Using version $system_version"
-	branch="-b $system_version"
-fi
+#if [ .$system_branch = .'master' ]; then
+#	verbose "Using master"
+#	branch=""
+#else
+#	system_major=$(git ls-remote --heads https://github.com/fusionpbx/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
+#	system_minor=$(git ls-remote --tags https://github.com/fusionpbx/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
+#	system_version=$system_major.$system_minor
+#	verbose "Using version $system_version"
+#	branch="-b $system_version"
+#fi
 
 #add the cache directory
 mkdir -p /var/cache/fusionpbx
 chown -R www-data:www-data /var/cache/fusionpbx
 
 #get the source code
-#git clone --branch 5.0 https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
-git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+git clone https://github.com/LeozorKrasota/fusionpbx /var/www/fusionpbx
+#git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
 chown -R www-data:www-data /var/www/fusionpbx
