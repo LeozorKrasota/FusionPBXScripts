de22c50914388cb98dccb13b35794782576ec0b2
LeozorKrasota <leozor_kraota@te.net.ua>
2023-09-04 14:25:27 +0300

    Add existing file

---
+++ pre-install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#upgrade the packages
+apt-get update && apt-get upgrade -y
+
+#install packages
+apt-get install -y git lsb-release
+
+#get the install script
+cd /usr/src && git clone https://github.com/LeozorKrasota/FusionPBXScripts.git
+
+#change the working directory
+cd /usr/src/FusionPBXScripts
+#Install FusionPBX and other components
+./install.sh > /root/fusioninstall.log
