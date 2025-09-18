657c84aedbe169d96126eaeba576e60adcda00ce
LeozorKrasota <118885914+LeozorKrasota@users.noreply.github.com>
2025-09-18 16:34:03 +0300

    Update config.sh

--- resources/config.sh
+++ resources/config.sh
@@ -9,23 +9,23 @@ system_branch=master                        # master, stable
 switch_branch=stable                        # master, stable
 switch_source=true                          # true (source compile) or false (binary package)
 switch_package=false                        # true (binary package) or false (source compile)
-switch_version=1.10.9                       # which source code to download, only for source
+switch_version=1.10.12                      # which source code to download, only for source
 switch_tls=true                             # true or false
 switch_token=                               # Get the auth token from https://signalwire.com
                                             # Signup or Login -> Profile -> Personal Auth Token
 # Sofia-Sip Settings
-sofia_version=1.13.13                        # release-version for sofia-sip to use
+sofia_version=1.13.17                       # release-version for sofia-sip to use
 
 # Database Settings
 database_name=fusionpbx                     # Database name (safe characters A-Z, a-z, 0-9)
 database_username=fusionpbx                 # Database username (safe characters A-Z, a-z, 0-9)
 database_password=random                    # random or a custom value (safe characters A-Z, a-z, 0-9)
 database_repo=system                        # PostgreSQL official, system
-database_version=13                         # requires repo official
+database_version=17                         # requires repo official
 database_host=127.0.0.1                     # hostname or IP address
 database_port=5432                          # port number
 database_backup=false                       # true or false
 
 # General Settings
-php_version=8.1                             # PHP version 7.1, 7.3, 7.4, 8.1
+php_version=8.2                             # PHP version 7.1, 7.3, 7.4, 8.1
 letsencrypt_folder=true                     # true or false
