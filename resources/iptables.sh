cf79763fcc766dd577517d3d0aa36d45e50e1620
LeozorKrasota <118885914+LeozorKrasota@users.noreply.github.com>
2025-09-19 11:46:45 +0300

    Update iptables.sh

--- resources/iptables.sh
+++ resources/iptables.sh
@@ -75,7 +75,9 @@ iptables -A INPUT -j DROP -p udp --dport 5060:5091 -m string --string "exec." --
 iptables -A INPUT -j DROP -p tcp --dport 5060:5091 -m string --string "exec." --algo bm --icase
 iptables -A INPUT -j DROP -p udp --dport 5060:5091 -m string --string "multipart/mixed;boundary" --algo bm --icase
 iptables -A INPUT -j DROP -p tcp --dport 5060:5091 -m string --string "multipart/mixed;boundary" --algo bm --icase
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s 195.138.67.124/32 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s 195.138.67.125/32 -p tcp -m tcp --dport 22 -j ACCEPT
+iptables -A INPUT -s 195.138.66.127/32 -p tcp -m tcp --dport 22 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 iptables -A INPUT -p tcp --dport 7443 -j ACCEPT
