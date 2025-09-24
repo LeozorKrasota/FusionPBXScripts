1439354ccf113ba9501e23be78ae739158ca1306
LeozorKrasota <118885914+LeozorKrasota@users.noreply.github.com>
2025-09-24 11:47:10 +0300

    Update config.sh

--- resources/config.sh
+++ resources/config.sh
@@ -31,9 +31,9 @@ php_version=8.2                             # PHP version 8.3, 8.2, 8.1
 letsencrypt_folder=true                     # true or false
 
 # Optional Applications
-application_transcribe=true                # Speech to Text
-application_speech=true                    # Text to Speech
-application_device_logs=true               # Log device provision requests
-application_dialplan_tools=false           # Add additional dialplan applications
-application_edit=false                     # Editor for XML, Provision, Scripts, and PHP
-application_sip_trunks=false               # Registration-based SIP trunks
+#application_transcribe=true                # Speech to Text
+#application_speech=true                    # Text to Speech
+#application_device_logs=true               # Log device provision requests
+#application_dialplan_tools=false           # Add additional dialplan applications
+#application_edit=false                     # Editor for XML, Provision, Scripts, and PHP
+#application_sip_trunks=false               # Registration-based SIP trunks
